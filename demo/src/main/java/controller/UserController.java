c4450879027f06320a936db31e938f4ccbab9d30
anyaguuu <ag156@rice.edu>
2023-07-25 11:03:51 -0700

    rest controller

--- demo/src/main/java/controller/UserController.java
+++ demo/src/main/java/controller/UserController.java
@@ -1,13 +1,29 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.UserRepository;
+import beans.User;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
-    // private final UserRepository userRepo;
+    private final UserRepository userRepo;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepo.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepo.save(user);
+    }
 }
