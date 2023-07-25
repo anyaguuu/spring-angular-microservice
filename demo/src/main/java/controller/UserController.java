9365757e1faece44385b671e6e0077c350faa2c6
anyaguuu <ag156@rice.edu>
2023-07-25 11:07:57 -0700

    rename

--- demo/src/main/java/controller/UserController.java
+++ demo/src/main/java/controller/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import app.UserRepository;
 import beans.User;
+import repo.UserRepository;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200") // deploy frontend
--- demo/src/main/java/repo/UserRepository.java
+++ demo/src/main/java/repo/UserRepository.java
@@ -1,4 +1,4 @@
-package app;
+package repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
