fc7da100bca5b593b8fd713b0ee78c05db90577e
anyaguuu <ag156@rice.edu>
2023-07-25 10:59:30 -0700

    JPA entity class

--- demo/src/main/java/beans/User.java
+++ demo/src/main/java/beans/User.java
@@ -1,6 +1,15 @@
 package beans;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
     /* User variables */
     private Long id;
