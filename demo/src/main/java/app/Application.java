5415d3b83d62a3d32b7f4eaae92ac1647f1836de
anyaguuu <ag156@rice.edu>
2023-07-25 11:11:27 -0700

    constructors + application

--- demo/src/main/java/app/Application.java
+++ demo/src/main/java/app/Application.java
@@ -1,7 +1,14 @@
 package app;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import beans.User;
+import repo.UserRepository;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +16,15 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepo) {
+		return args -> {
+			Stream.of("Anya", "Rachel", "Phoebe", "Ross", "Chandler", "Joey").forEach(name -> {
+				User user = new User(name);
+				userRepo.save(user);
+			});
+			userRepo.findAll().forEach(System.out::println);
+			}
 
 }
